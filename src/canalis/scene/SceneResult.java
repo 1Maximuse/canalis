eea6a2d93ca5d9806264500efc1de3e4fa6858e1
Emmanuel <geofanny.emmanuel@gmail.com>
2019-12-11 15:35:27 +0700

    fixed randomizing bug

--- src/canalis/scene/SceneResult.java
+++ src/canalis/scene/SceneResult.java
@@ -14,14 +14,15 @@ public class SceneResult extends Scene {
 
 	private final String[] result = {"You Did It !", "Well Done !", "Good Job !"};
 	private final Random rng;
+	private final TextCentered win;
 	private final TextCentered time;
-	private int random = 0;
+	private int random;
 	
 	public SceneResult(Game game, Display display) {
 		super(game, display);
 		rng = new Random();
-		randomize();
-		addSceneObject(new TextCentered(display, 150, new Font("Algerian", Font.PLAIN, 70), Color.BLACK, result[random]));
+		random = rng.nextInt(3);
+		addSceneObject(win = new TextCentered(display, 150, new Font("Algerian", Font.PLAIN, 70), Color.BLACK, result[random]));
 		addSceneObject(time = new TextCentered(display, 300, new Font("Segoe Print", Font.PLAIN, 40), Color.BLACK, ""));
 		addSceneObject(new ButtonChangeScene(display, (display.getWidth()/3*1)-(200/2), (display.getHeight()/3*2), 200, 80, 1, Assets.retry));
 		addSceneObject(new ButtonChangeScene(display, (display.getWidth()/3*2)-(200/2), (display.getHeight()/3*2), 200, 80, 0, Assets.toMainMenu));
@@ -29,6 +30,7 @@ public class SceneResult extends Scene {
 	
 	public void randomize() {
 		random = rng.nextInt(3);
+		win.setText(result[random]);
 	}
 
 	public void setTime(int minute, int second) {
